7549ae957f3bfe7587b807b5894111095f16f6a7
Seth Caldwell <caldwellst@gmail.com>
2022-02-02 10:38:33 +0000

    Test analysis on Libya

---
+++ 02_analysis/analysis.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+
+# Sample analysis to test our understanding
+# of what needs to be done
+
+df <- read_csv(
+  file.path(
+    Sys.getenv("JIAF_DATA_DIR"),
+    "Data aggregated for analysis",
+    "lby_pins_2022.csv"
+  )
+)
+
+df %>%
+  mutate(group = case_when(
+    sector == "intersectoral" ~ "intersectoral",
+    source == "ocha" ~ "sectoral",
+    source == "cluster" ~ "sectoral_cluster"
+  )) %>%
+  group_by(
+    group,
+    adm3_pcode,
+    population_group,
+    sex,
+    age
+  ) %>%
+  summarize(
+    pin = max(pin),
+    .groups = "drop"
+  ) %>%
+  group_by(group) %>%
+  summarize(
+    pin = sum(pin),
+    .groups = "drop"
+  )
