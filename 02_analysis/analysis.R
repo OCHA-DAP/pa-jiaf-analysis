5472420fc90adde7cfb8d121c6d8755bb66fc906
Seth Caldwell <caldwellst@gmail.com>
2022-02-02 10:58:40 +0000

    Add at key drivers of Libya PiN

--- 02_analysis/analysis.R
+++ 02_analysis/analysis.R
@@ -11,7 +11,7 @@ df <- read_csv(
   )
 )
 
-df %>%
+max_df <- df %>%
   mutate(group = case_when(
     sector == "intersectoral" ~ "intersectoral",
     source == "ocha" ~ "sectoral",
@@ -25,11 +25,43 @@ df %>%
     age
   ) %>%
   summarize(
-    pin = max(pin),
+    max_pin = max(pin),
+    max_sector = paste(sector[pin == max_pin], collapse = ", "),
     .groups = "drop"
-  ) %>%
+  ) 
+
+# analysis example
+
+df_sample <- max_df %>%
   group_by(group) %>%
   summarize(
-    pin = sum(pin),
+    pin = sum(max_pin),
     .groups = "drop"
   )
+
+df_sample
+
+# driving sectors
+
+df_drivers <- max_df %>%
+  filter(group != "intersectoral") %>%
+  group_by(group, max_sector) %>%
+  summarize(
+    max_count = n(),
+    pin = sum(max_pin),
+    .groups = "drop_last"
+  ) %>%
+  arrange(desc(pin), .by_group = TRUE) %>%
+  mutate(
+    max_count_pct = scales::percent(max_count / sum(max_count), accuracy = 1),
+    pin_pct = scales::percent(pin / sum(pin), accuracy = 1)
+  )
+
+# df_drivers %>%
+#   write_csv(
+#     file.path(
+#       Sys.getenv("JIAF_DATA_DIR"),
+#       "Data analyzed",
+#       "lby_sample_drivers.csv"
+#     )
+#   )
