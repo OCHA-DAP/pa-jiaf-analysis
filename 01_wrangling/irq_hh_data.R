fe1388872a064e5756847680fecf4aa9e7e08d5c
Rawa Ganjani <rawaomer@outlook.com>
2022-05-23 17:35:56 +0300

    adding somalia and syria

--- 01_wrangling/irq_hh_data.R
+++ 01_wrangling/irq_hh_data.R
@@ -28,8 +28,12 @@ read_in_disagg <- function(fp) {
         adm2_pcode = admin2Pcode,
         adm1_name = gov_name,
         adm2_name = dist_name,
-        population_group = .x,
-        pop_num
+        population_group = case_when(
+          .x == "In-Camp-IDPs" ~ "idp_in",
+          .x == "Out-of-Camp-IDPs" ~ "idp_out",
+          .x == "Returnees" ~ "ret"
+        ),
+        target_population = pop_num
       )
   )
 }
@@ -53,8 +57,10 @@ ocha_fp <- file.path(
 df <- read_excel(
   ocha_fp,
   sheet = "data",
-  col_names = FALSE
-)
+  skip = 1
+) %>%
+  clean_names() %>%
+  type_convert()
 
 population_fp <- file.path(
   file_paths$ocha_dir,
@@ -65,55 +71,41 @@ population_fp <- file.path(
 )
 
 df_irq_pops <- read_in_disagg(population_fp) %>%
-  mutate(pop_num = ifelse(population_group == "In-Camp-IDPs" & adm2_name == "Al-Falluja", 2500, pop_num)) %>%
-  filter(pop_num != 0 & adm1_name != "Total") %>%
-  select(adm2_name, population_group, pop_num)
-
-#list of indicators
-list_indicators <- df %>% 
-  filter(row_number() < 3) %>% 
-  t() %>%
-  data.frame(row.names = NULL) %>%
-  rename(indicator_name = X1, indicator_code = X2) %>%
-  filter(grepl("^s[0-9]", indicator_code))
-
-#renaming the dataset
-names(df) <- df[2, ]
+  mutate(
+    target_population = ifelse(
+      population_group == "idp_in" &
+        adm2_name == "Al-Falluja",
+      2500,
+      target_population
+    )
+  ) %>%
+  filter(target_population != 0 & adm1_name != "Total") %>%
+  select(adm2_name, population_group, target_population)
 
 df_cleaned <- df %>% 
-  filter(row_number() > 2) %>%
-  clean_names() %>% 
-  type_convert() %>%
   mutate(
-    hh_id = paste0("IRQ", 1:nrow(.))
+    hh_id = paste0("IRQ", 1:nrow(.)),
+    population_group = case_when(
+      population_group == "idp_in_camp" ~ "idp_in",
+      population_group == "idp_out_camp" ~ "idp_out",
+      population_group == "returnee" ~ "ret"
+    )
   ) %>%
   pivot_longer(cols = matches("^s[0-9]"),
                names_to = "indicator",
                values_to = "severity",
                values_drop_na = TRUE) %>%
-  left_join(list_indicators, by=c("indicator" = "indicator_code")) %>%
   left_join(df_irq_pops, by = c("dist_cod" = "adm2_name", "population_group")) %>%
   transmute(
     hh_id,
-    adm0_name = "IRAQ",
-    adm0_pcode = "IRQ",
-    adm1_name = gov_cod,
-    adm1_pcode = gov_cod_pc,
-    adm2_name = dist_cod,
-    adm2_pcode = dist_cod_pc,
-    population_group = case_when(
-      population_group == "idp_in_camp" ~ "In-Camp-IDPs",
-      population_group == "idp_out_camp" ~ "Out-of-Camp-IDPs",
-      population_group == "returnee" ~ "Returnees"
-    ),
-    target_population = df_irq_pops$pop_num[match(
-      paste0(adm2_name, population_group),
-      paste0(df_irq_pops$adm2_name, df_irq_pops$population_group)
-    )],
+    adm0_name = "Iraq",
+    area = dist_cod,
+    population_group,
+    target_population,
     sector = case_when(
       indicator %in% c("s01", "s11", "s12", "s13") ~ "protection",
       indicator == "s02" ~ "education",
-      indicator %in% c("s03", "s06") ~ "livelihoods emergency",
+      indicator %in% c("s03", "s06") ~ "emergency recovery",
       indicator %in% c("s04", "s05") ~ "food security",
       indicator %in% c("s07", "s08") ~ "health",
       indicator == "s09" ~ "child protection",
@@ -121,8 +113,7 @@ df_cleaned <- df %>%
       indicator %in% c("s14", "s15") ~ "SNFI",
       indicator %in% c("s16", "s17", "s18") ~ "WASH"
     ),
-    indicator_label = indicator_name,
-    indicator,
+    indicator = as.numeric(gsub("s", "", indicator)),
     severity,
     weight
   )
--- 02_analysis/hh_data_analysis.R
+++ 02_analysis/hh_data_analysis.R
@@ -1,4 +1,4 @@
-rm(list=ls(all=T))
+rm(list = ls(all = T))
 library(tidyverse)
 library(readxl)
 library(janitor)
@@ -17,69 +17,48 @@ file_paths <- get_paths_analysis()
 #### HH Aggregation Method ####
 ###############################
 
-df <- map_dfr(list.files(file.path(file_paths$input_dir, "hh_data"), full.names = TRUE), read_csv)
+df <-
+  map_dfr(list.files(file.path(file_paths$input_dir, "hh_data"), full.names = TRUE), read_csv)
 
 hh_scoring_method_1 <- df %>%
-  group_by(
-    hh_id
-  ) %>%
-  slice_max(order_by = severity, prop = 0.25, with_ties = FALSE) %>%
-  summarize(
-    mean_max_25 = mean(severity),
-    median_max_25 = median(severity)
-  )
+  group_by(hh_id) %>%
+  slice_max(order_by = severity,
+            prop = 0.25,
+            with_ties = FALSE) %>%
+  summarize(mean_max_25 = mean(severity),
+            median_max_25 = median(severity))
 
 hh_scoring_method_2 <- df %>%
-  group_by(
-    hh_id
-  ) %>%
-  slice_max(order_by = severity, prop = 0.5, with_ties = FALSE) %>%
-  summarize(
-    mean_max_50 = mean(severity),   
-    median_max_50 = median(severity)
-  )
+  group_by(hh_id) %>%
+  slice_max(order_by = severity,
+            prop = 0.5,
+            with_ties = FALSE) %>%
+  summarize(mean_max_50 = mean(severity),
+            median_max_50 = median(severity))
 
 hh_scoring_method_3 <- df %>%
-  group_by(
-    hh_id
-  ) %>%
-  slice_max(order_by = severity, prop = 0.75, with_ties = FALSE) %>%
-  summarize(
-    mean_max_75 = mean(severity),
-    median_max_75 = median(severity)
-  )
+  group_by(hh_id) %>%
+  slice_max(order_by = severity,
+            prop = 0.75,
+            with_ties = FALSE) %>%
+  summarize(mean_max_75 = mean(severity),
+            median_max_75 = median(severity))
 
 hh_scoring_method_4 <- df %>%
-  group_by(
-    hh_id
-  ) %>%
-  summarize(
-    mean_all = mean(severity),
-    median_all = median(severity)
-  )
+  group_by(hh_id) %>%
+  summarize(mean_all = mean(severity),
+            median_all = median(severity))
 
 hh_scoring_method_5 <- df %>%
-  group_by(
-    hh_id,
-    sector
-  ) %>%
-  summarize(
-    hh_sectoral_mean = mean(severity)
-  ) %>%
-  group_by(
-    hh_id
-  ) %>%
-  summarize(
-    hh_sectoral_max_mean = max(hh_sectoral_mean)
-  )
+  group_by(hh_id,
+           sector) %>%
+  summarize(hh_sectoral_mean = mean(severity)) %>%
+  group_by(hh_id) %>%
+  summarize(hh_sectoral_max_mean = max(hh_sectoral_mean))
 
 hh_scoring_method_6 <- df %>%
-  group_by(
-    hh_id
-  ) %>%
-  summarize(
-    any_indicator_inneed = sum(severity >= 3, na.rm = T)
-  ) %>%
+  group_by(hh_id) %>%
+  summarize(any_indicator_inneed = sum(severity >= 3, na.rm = T)) %>%
   mutate(
     two_indicator_inneed = ifelse(any_indicator_inneed > 1, 3, 0),
     any_indicator_inneed = ifelse(any_indicator_inneed > 0, 3, 0)
@@ -92,18 +71,13 @@ hh_scoring_method <-
   left_join(hh_scoring_method_4) %>%
   left_join(hh_scoring_method_5) %>%
   left_join(hh_scoring_method_6) %>%
-  pivot_longer(
-    cols = -hh_id,
-    values_to = "severity",
-    names_to = "aggregation_method"
-  ) %>%
-  left_join(
-    unique(select(
-    df,-c(severity, sector, indicator_label, indicator))
-    ), by = "hh_id") %>%
-  mutate(
-    severity = ifelse(severity >= 3, 1, 0)
-  )
+  pivot_longer(cols = -hh_id,
+               values_to = "severity",
+               names_to = "aggregation_method") %>%
+  left_join(unique(select(df, -c(
+    severity, sector, indicator
+  ))), by = "hh_id") %>%
+  mutate(severity = ifelse(severity >= 3, 1, 0))
 
 #freeing up space of ram
 remove(
@@ -116,20 +90,15 @@ remove(
 )
 
 hh_summarized <- hh_scoring_method %>%
-  group_by(
-    adm2_name,
-    population_group,
-    target_population,
-    aggregation_method
-  ) %>%
-  summarize(
-    percentage = weighted.mean(severity, w = weight),
-    .groups = "drop"
-  ) %>%
-  mutate(
-    value = round(percentage * target_population),
-    calculation_level = "from household indicators' severity scores"
-  ) %>%
+  group_by(adm0_name,
+           area,
+           population_group,
+           target_population,
+           aggregation_method) %>%
+  summarize(percentage = weighted.mean(severity, w = weight),
+            .groups = "drop") %>%
+  mutate(value = round(percentage * target_population),
+         calculation_level = "from household indicators' severity scores") %>%
   select(-c(target_population, percentage))
 
 ####################################
@@ -137,33 +106,30 @@ hh_summarized <- hh_scoring_method %>%
 ####################################
 
 area_summarized <- df %>%
-  group_by(adm2_name,
+  group_by(adm0_name,
+           area,
            population_group,
            target_population,
            sector,
-           indicator_label,
            indicator) %>%
   mutate(severity = ifelse(severity >= 3, 1, 0)) %>%
   summarize(percentage = weighted.mean(severity, w = weight)) %>%
-  mutate(value = round(percentage * target_population)) 
+  mutate(value = round(percentage * target_population))
 
 area_pin_from_sector <- area_summarized %>%
-  group_by(adm2_name,
+  group_by(adm0_name,
+           area,
            population_group,
            sector) %>%
-  summarize(
-    area_pin_sector_mean = round(mean(value, na.rm = TRUE))
-  ) %>%
-  group_by(
-    adm2_name,
-    population_group
-  ) %>%
-  summarize(
-    area_pin_sector_mean = max(area_pin_sector_mean, na.rm = TRUE)
-  )
+  summarize(area_pin_sector_mean = round(mean(value, na.rm = TRUE))) %>%
+  group_by(adm0_name,
+           area,
+           population_group) %>%
+  summarize(area_pin_sector_mean = max(area_pin_sector_mean, na.rm = TRUE))
 
 area_pin_from_indicator <- area_summarized %>%
-  group_by(adm2_name,
+  group_by(adm0_name,
+           area,
            population_group) %>%
   summarize(
     area_pin_indicator_max = max(value, na.rm = TRUE),
@@ -174,61 +140,104 @@ area_pin <-
   left_join(
     area_pin_from_indicator,
     area_pin_from_sector,
-    by = c("adm2_name", "population_group")
+    by = c("adm0_name", "area", "population_group")
   ) %>%
   pivot_longer(
-    cols = matches("^area"),
+    cols = matches("^area_pin"),
     values_to = "value",
     names_to = "aggregation_method"
   ) %>%
-  filter(
-    !is.infinite(value) & !is.nan(value)
-  ) %>%
-  mutate(
-    calculation_level = "indicators' or sectors' PiN at area level"
-  )
+  filter(!is.infinite(value) & !is.nan(value)) %>%
+  mutate(calculation_level = "indicators' or sectors' PiN at area level")
 
-df_hno_pin <- read.csv(paste0(file_paths$input_dir, "/irq_pins_2022.csv")) %>%
+df_irq_ocha_pin <-
+  read.csv(paste0(file_paths$input_dir, "/irq_pins_2022.csv"))
+
+df_irq_hno_pin <- df_irq_ocha_pin %>%
+  filter(sector == "itc") %>%
+  group_by(adm0_name) %>%
+  summarize(value = sum(pin, na.rm = TRUE)) %>%
+  mutate(aggregation_method = "HNO 2022 Intersectoral PiN",
+         calculation_level = "HNO 2022 Intersectoral PiN")
+
+df_irq_cluster_pin <- df_irq_ocha_pin %>%
   filter(sector != "itc") %>%
-  group_by(
-    adm2_name,
-    population_group
-  ) %>%
-  summarize(
-    value = max(pin, na.rm = TRUE)
-  ) %>% 
-  mutate(
-    aggregation_method = "HNO 2022 Intersectoral PiN (without expert judgement)",
-    calculation_level = "HNO 2022 Intersectoral PiN"
-  )
+  group_by(adm0_name,
+           adm2_name,
+           population_group) %>%
+  summarize(value = max(pin, na.rm = TRUE)) %>%
+  group_by(adm0_name) %>%
+  summarize(value = sum(value, na.rm = T)) %>%
+  mutate(aggregation_method = "Max of Cluster calculated PiN",
+         calculation_level = "Max of Cluster calculated PiN")
 
-pin_all <- rbind(
-  area_pin,
-  hh_summarized,
-  df_hno_pin
-) 
+df_syr_ocha_pin <-
+  read.csv(paste0(file_paths$input_dir, "/syr_pins_2022.csv"))
+
+df_syr_hno_pin <- df_syr_ocha_pin %>%
+  filter(sector == "intersectoral") %>%
+  group_by(adm0_name) %>%
+  summarize(value = sum(pin, na.rm = TRUE)) %>%
+  mutate(aggregation_method = "HNO 2022 Intersectoral PiN",
+         calculation_level = "HNO 2022 Intersectoral PiN")
+
+df_syr_cluster_pin <- df_syr_ocha_pin %>%
+  filter(sector != "intersectoral") %>%
+  group_by(adm0_name,
+           adm3_name) %>%
+  summarize(value = max(pin, na.rm = TRUE)) %>%
+  group_by(adm0_name) %>%
+  summarize(value = sum(value, na.rm = T)) %>%
+  mutate(aggregation_method = "Max of Cluster calculated PiN",
+         calculation_level = "Max of Cluster calculated PiN")
+
+df_som_ocha_pin <-
+  read.csv(paste0(file_paths$input_dir, "/som_pins_2022.csv"))
+
+df_som_hno_pin <- df_som_ocha_pin %>%
+  filter(sector == "intersectoral") %>%
+  group_by(adm0_name) %>%
+  summarize(value = sum(pin, na.rm = TRUE)) %>%
+  mutate(aggregation_method = "HNO 2022 Intersectoral PiN",
+         calculation_level = "HNO 2022 Intersectoral PiN")
+
+df_som_cluster_pin <- df_som_ocha_pin %>%
+  filter(sector != "intersectoral") %>%
+  group_by(adm0_name,
+           adm2_name,
+           population_group) %>%
+  summarize(value = max(pin, na.rm = TRUE)) %>%
+  group_by(adm0_name) %>%
+  summarize(value = sum(value, na.rm = T)) %>%
+  mutate(aggregation_method = "Max of Cluster calculated PiN",
+         calculation_level = "Max of Cluster calculated PiN")
 
 #filtering the population to only area/population groups that have PiNs
-df_irq_pops <- df %>%
-  group_by(
-    adm2_name,
-    population_group
-  ) %>%
-  summarize(
-    value = max(target_population, na.rm = TRUE)
-  ) %>%
+df_pops <- df %>%
+  group_by(adm0_name,
+           area,
+           population_group,
+           drop = TRUE) %>%
+  summarize(value = max(target_population, na.rm = TRUE)) %>%
   mutate(aggregation_method = "Total Targetted Population",
          calculation_level = "Total Targetted Population")
 
-pin_all <- pin_all %>%
-  rbind(df_irq_pops) %>%
-  group_by(
-    adm0_name = "Iraq",
-    calculation_level,
-    aggregation_method
-  ) %>%
-  summarize(
-    value = sum(value, na.rm = TRUE)
+pin_all <-
+  rbind(df_pops,
+        area_pin,
+        hh_summarized) %>%
+  group_by(adm0_name,
+           calculation_level,
+           aggregation_method,
+           .drop = TRUE) %>%
+  summarize(value = sum(value, na.rm = TRUE)) %>%
+  rbind(
+    df_syr_hno_pin,
+    df_irq_hno_pin,
+    df_som_hno_pin,
+    df_syr_cluster_pin,
+    df_irq_cluster_pin,
+    df_som_cluster_pin
   ) %>%
   mutate(
     aggregation_method = case_when(
@@ -247,55 +256,62 @@ pin_all <- pin_all %>%
       aggregation_method == "area_pin_indicator_mean" ~ "mean of all indicators' PiN",
       aggregation_method == "area_pin_sector_mean" ~ "mean of all sectors' PiN",
       TRUE ~ aggregation_method
-    )
-  )
+    ),
+    value = round(value / 1000000, 2)
+  ) %>%
+  arrange(adm0_name, desc(value))
 
-#importing roboto fonts
-hrbrthemes::import_roboto_condensed()
-
-bar_chart <- ggplot(pin_all, aes(
-  fill = fct_rev(calculation_level), y = value, x = reorder(aggregation_method, +value),
-  label = paste0(round(value/1000000, 2), "M")
-)) +
+bar_chart <-
+  ggplot(pin_all,
+         aes(
+           fill = calculation_level,
+           y = value,
+           x = reorder(aggregation_method,+value),
+           label = paste0(value, "M")
+         )) +
+  facet_wrap( ~ adm0_name, strip.position = "bottom", scales = "free_x") +
   geom_col() +
-  geom_text(position = position_dodge(width = .3),hjust=-.3, size = 4)+
+  geom_text(position = position_identity(),
+            hjust = -.2,
+            size = 4) +
   coord_flip() +
-  labs(
-    fill = "PiN Calculation",
-    x = "",
-    y = "PIN",
-    title = "Option3: Comparison of HH  severity aggregation vs indicator pin vs sectoral pin from Iraq MSNA dataset"
-  ) +
+  labs(fill = "",
+       x = "",
+       y = "PIN",
+       title = "Option3: Comparison of HH  severity aggregation vs indicator pin vs sectoral pin from Iraq MSNA dataset") +
   theme_light() +
-  scale_y_continuous(label = scales::comma)+
-  scale_x_discrete(labels = function(x) str_wrap(x, width = 30)) +
-  scale_fill_manual(values = c("#B00020", "#D1C4E9", "#00E676", "#FFE0B2")) +
+  scale_y_continuous(labels = scales::comma, expand = expansion(c(0, .2))) +
+  scale_x_discrete(
+    labels = function(x)
+      str_wrap(x, width = 35)
+  ) +
+  scale_fill_manual(values = c("#FFE0B2", "#00E676", "#D1C4E9", "#2154FF", "#B00020")) +
   theme(
-    plot.title = element_text(face = "bold", size = 22, colour = "#134373", margin = margin(10, 10, 30, 10, "pt")),
-    axis.text = element_text(face = "bold", size = 14),
+    plot.title = element_text(
+      face = "bold",
+      size = 22,
+      colour = "#134373",
+      margin = margin(10, 10, 30, 10, "pt")
+    ),
+    axis.text = element_text(face = "bold", size = 10),
     legend.text = element_text(size = 12),
-    legend.title = element_text(size = 13, face = "bold"),
-    legend.position = c(.85, .1),
-    panel.background = element_rect(fill='transparent'),
+    legend.position = "bottom",
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank(),
-    legend.background = element_rect(fill='transparent'),
-    legend.box.background = element_rect(fill='transparent')
-    )
+    panel.background = element_rect(fill = 'transparent'),
+    legend.background = element_rect(fill = 'transparent'),
+    legend.box.background = element_rect(fill = 'transparent'),
+    strip.text = element_text(size = 16)
+  )
 
-
-ggsave(file.path(
-  file_paths$output_dir,
-  "2022_hh_data_aggregation.png"
-),
-bg = "transparent",
-height = 13,
-width = 20, 
-plot = bar_chart
+ggsave(
+  file.path(file_paths$output_dir,
+            "2022_hh_data_aggregations.png"),
+  bg = "transparent",
+  height = 13,
+  width = 25,
+  plot = bar_chart
 )
 
-
-write_csv(
-  pin_all,
-  file.path(file_paths$output_dir, "/2022_hh_data_aggregated_pin.csv")
-)
+write_csv(pin_all,
+          file.path(file_paths$output_dir, "/2022_hh_data_aggregated_pin.csv"))
